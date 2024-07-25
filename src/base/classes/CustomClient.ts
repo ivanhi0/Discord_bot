1f20b24a1cb81305738fe14fcc8a633fd79d6ff7
idbod <id.bodrov@gmail.com>
2024-07-25 23:22:25 +0300

    Commit

---
+++ src/base/classes/CustomClient.ts
@@ -0,0 +1,6 @@
+import { Client } from "discord.js";
+import ICustomClient from "../interfaces/ICustomClient";
+import IConfig from "../interfaces/IConfig";
+import default class CustomClient extends Client {
+    
+}
